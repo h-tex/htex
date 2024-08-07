2f29ae9d6e37022696a5407767ec0157ddffc96b
Lea Verou <lea@verou.me>
2024-08-07 03:02:46 +0300

    reading_time: Strip all tags, display hours for min > 60

--- src/filters.js
+++ src/filters.js
@@ -2,15 +2,26 @@ import readingTime from "reading-time";
 export * from "./filters/urls.js";
 export * from "./filters/format.js";
 
-export function reading_time (text) {
+export function reading_time (text, {wpm = 350} = {}) {
 	if (!text) {
 		return;
 	}
 
-	// Strip HTML comments
-	text = text.replace(/<!--.*?-->/gs, "");
+	// https://github.com/mozilla/nunjucks/blob/ea0d6d5396d39d9eed1b864febb36fbeca908f23/nunjucks/src/filters.js#L491
+	let tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>|<!--[\s\S]*?-->/gi;
 
-	return readingTime(text, {
-		wordsPerMinute: 350
+	// Strip HTML comments & tags
+	text = text.replaceAll(tags, "");
+
+	let ret = readingTime(text, {
+		wordsPerMinute: wpm,
 	});
+
+	if (ret.minutes > 60) {
+		ret.hours = Math.floor(ret.minutes / 60);
+		ret.minutes = ret.minutes % 60;
+		ret.text = `${ ret.hours } hr ${ Math.ceil(ret.minutes) } min read`;
+	}
+
+	return ret;
 }
