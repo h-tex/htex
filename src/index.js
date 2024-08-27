9ef1c3185692423418555444a45ccc10475ea765
Lea Verou <lea@verou.me>
2024-08-28 00:45:05 +0300

    Split 11ty plugins into separate file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import md from "./markdown.js";
-import citations from "eleventy-plugin-citations";
-import outline from "eleventy-plugin-outline";
-import todos from "./todos.js";
-import scoped from "./scoped.js";
-import imageDimensions from "./image.js";
+import * as plugins from "./plugins.js";
 import * as filters from "./filters.js";
-
 
 export default function (config, options = {}) {
 	for (let name in filters) {
@@ -16,24 +11,10 @@ export default function (config, options = {}) {
 
 	config.addPlugin(md, options.markdown);
 
-	if (options.citations !== false) {
-		config.addPlugin(citations, options.citations);
-	}
-
-	if (options.outline !== false) {
-		config.addPlugin(outline, options.outline);
-	}
-
-	if (options.imageDimensions !== false) {
-		config.addPlugin(imageDimensions);
-	}
-
-	if (options.todos !== false) {
-		config.addPlugin(todos, options.todos);
-	}
-
-	if (options.scoped !== false) {
-		config.addPlugin(scoped, options.scoped);
+	for (let pluginId in plugins) {
+		if (options[pluginId] !== false) {
+			config.addPlugin(plugins[pluginId], options[pluginId]);
+		}
 	}
 
 	if (options.serverPort) {
---
+++ src/plugins.js
@@ -0,0 +1,5 @@
+export { default as citations } from "eleventy-plugin-citations";
+export { default as outline } from "eleventy-plugin-outline";
+export { default as todos } from "./todos.js";
+export { default as scoped } from "./scoped.js";
+export { default as imageDimensions } from "./image.js";
